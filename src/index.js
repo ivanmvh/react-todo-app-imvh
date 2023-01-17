2ef0216f83f6241fc9b8b377f338d9804458d073
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-17 00:25:23 -0500

    Fix error ReactDOM is not supported replace with creteRoot

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react"
-import ReactDOM from "react-dom"
-// component file
+import ReactDOM from "react-dom/client"
+//component file
 import TodoContainer from "./components/TodoContainer"
-
-ReactDOM.render(
-  <React.StrictMode>
-    <TodoContainer />
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+      <TodoContainer />
+    </React.StrictMode>
+  )

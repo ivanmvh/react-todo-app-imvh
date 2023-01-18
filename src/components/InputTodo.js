2e52e2bff7f117bc728f8cc1ca64cb11a620ed42
Ivan Martinez von Halle <ivanmvh@gmail.com>
2023-01-18 05:53:59 -0500

    Validating the Form Submission - Generating unique id's

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class InputTodo extends Component {
   constructor(props) {
@@ -15,11 +16,25 @@ class InputTodo extends Component {
     });
   };
 
+  handleSubmit = (e) => {
+    const { title } = this.state;
+    const { addTodoProps } = this.props;
+    e.preventDefault();
+    if (title.trim()) {
+      addTodoProps(title);
+      this.setState({
+        title: '',
+      });
+    } else {
+      alert('Please write item');
+    }
+  };
+
   render() {
     const { title } = this.state;
 
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <input
           type="text"
           placeholder="Add todo..."
@@ -27,45 +42,14 @@ class InputTodo extends Component {
           name="title"
           onChange={this.onChange}
         />
-        <button type="button">Submit</button>
+        <button type="submit">Submit</button>
       </form>
     );
   }
 }
+
+InputTodo.propTypes = {
+  addTodoProps: PropTypes.func.isRequired,
+};
+
 export default InputTodo;
-
-/*
-import React, { Component } from 'react';
-
-class InputTodo extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      title: '',
-    };
-  }
-
-  onChange = (e) => {
-    this.setState({
-      title: e.target.value,
-    });
-  };
-
-  render() {
-    const { title } = this.state;
-    return (
-      <form>
-        <input
-          type="text"
-          placeholder="Add Todo..."
-          value={title}
-          onChange={this.onChange}
-        />
-        <button type="button">Submit</button>
-      </form>
-    );
-  }
-}
-export default InputTodo;
-*/
--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import TodosList from './TodosList';
 import Header from './Header';
 import InputTodo from './InputTodo';
@@ -9,17 +10,17 @@ class TodoContainer extends React.Component {
     this.state = {
       todos: [
         {
-          id: 1,
+          id: uuidv4(),
           title: 'Setup development environment 1',
           completed: true,
         },
         {
-          id: 2,
+          id: uuidv4(),
           title: 'Develop website and add content',
           completed: false,
         },
         {
-          id: 3,
+          id: uuidv4(),
           title: 'Deploy to live server',
           completed: false,
         },
@@ -50,12 +51,24 @@ class TodoContainer extends React.Component {
     });
   };
 
+  addTodoItem = (title) => {
+    const { todos } = this.state;
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
+    this.setState({
+      todos: [...todos, newTodo],
+    });
+  };
+
   render() {
     const { todos } = this.state;
     return (
       <div>
         <Header />
-        <InputTodo />
+        <InputTodo addTodoProps={this.addTodoItem} />
         <TodosList
           todos={todos}
           handleChangeProps={this.handleChange}
